536debb81bba8362b27f334cebe995fa147f16e3
Don Wong <wkydon@gmail.com>
2025-07-25 02:11:13 +0000

    Somehow the login page is like double the height of my screen, also the

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,23 +8,23 @@ export default async function LoginPage() {
   const settings = await getSettings();
 
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
+    <div className="w-full lg:grid lg:grid-cols-2 h-screen">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <LoginForm users={users} />
         </div>
       </div>
-      <div className="hidden bg-muted lg:block relative">
+      <div className="hidden lg:block relative">
         <Image
           src={settings.loginImageUrl}
           alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover"
+          layout="fill"
+          objectFit="cover"
           data-ai-hint="friends group"
         />
-        <div className="absolute inset-0 bg-gradient-to-r from-background/30 via-background/80 to-background"></div>
+        <div className="absolute inset-0 bg-gradient-to-l from-background/30 via-background/80 to-background"></div>
       </div>
     </div>
   );
 }
+
