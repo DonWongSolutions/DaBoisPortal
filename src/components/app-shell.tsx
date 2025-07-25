759db9866e484fe640935375693847b2586dbe3c
Don Wong <wkydon@gmail.com>
2025-07-25 02:12:02 +0000

    Error: `legacyBehavior` is deprecated and will be removed in a future re

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -96,15 +96,16 @@ function MainNav({ user }: { user: User }) {
         }
         return (
           <SidebarMenuItem key={item.href}>
-            <Link href={item.href} legacyBehavior passHref>
-              <SidebarMenuButton
-                isActive={pathname === item.href}
-                tooltip={item.label}
-              >
+            <SidebarMenuButton
+              asChild
+              isActive={pathname === item.href}
+              tooltip={item.label}
+            >
+              <Link href={item.href}>
                 <item.icon className="h-5 w-5" />
                 <span>{item.label}</span>
-              </SidebarMenuButton>
-            </Link>
+              </Link>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         );
       })}
