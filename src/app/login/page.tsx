b85d9cb94b6fa65148c87acc3dcaa908fb6b0a75
Don Wong <wkydon@gmail.com>
2025-07-25 07:00:24 +0000

    on the login page can we make the image take up more persent of the scre

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 import Image from 'next/image';
 import { LoginForm } from '@/components/login-form';
 import { getUsers, getSettings } from '@/lib/data';
@@ -7,13 +8,13 @@ export default async function LoginPage() {
   const settings = await getSettings();
 
   return (
-    <div className="w-full lg:grid lg:grid-cols-2 h-screen">
-      <div className="flex items-center justify-center py-12">
+    <div className="w-full lg:grid lg:grid-cols-4 h-screen">
+      <div className="flex items-center justify-center py-12 lg:col-span-1">
         <div className="mx-auto grid w-[350px] gap-6">
           <LoginForm users={users} />
         </div>
       </div>
-      <div className="hidden lg:block relative">
+      <div className="hidden lg:block relative lg:col-span-3">
         <Image
           src={settings.loginImageUrl}
           alt="Image"
